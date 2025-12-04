fac680694e05a37ae29e05d7e71a28a7baa553fa
Razif / Programm <razifbedula.official@gmail.com>
2025-12-04 23:06:56 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,49 +1,3 @@
-const {
-  default: makeWaSocket,
-  useMultiFileAuthState
-} = require("@whiskeysockets/baileys")
-const qrcode = require("qrcode-terminal")
-const pino = require("pino")
-let question = text => {
-  let rl = require("readline").createInterface({
-    input: process.stdin,
-    output: process.stdout
-  })
-  return new Promise(resolve => {
-    rl.question(text, resolve)
-  })
-}
-
-async function startBot() {
-  const { state, saveCreds } = await useMultiFileAuthState("session")
-  const conn = makeWaSocket({
-    auth: state,
-    logger: pino({ level: "silent" })
-  })
-  
-  conn.ev.on("creds.update", saveCreds)
-  conn.ev.on("connection.update", async update => {
-    const { connection, lastDisconnect, qr } = update
-    if (qr) {
-      const waNumber = await question("start with your number: ")
-      const code = await conn.requestPairingCode(waNumber)
-      console.log(code)
-      // qrcode.generate(qr, {small: true })
-    }
-    if (connection == "close") {
-      console.log("close, restart bot")
-      await console.log(lastDisconnect)
-      startBot()
-    }
-    if (connection == "open") {
-      console.log("connect bot: +" + conn.user.id)
-    }
-  })
-
-  conn.ev.on("messages.upsert", ({ messages }) => require("./messages.js")(conn, messages[0]))
-}
-startBot()
-=======
 // config
 prefixes = ["!", "#", "/", ".", ""]; // Array awalan yang dibenarkan
 ownerNumber = ""; // nombormu
@@ -354,4 +308,3 @@ async function startBot() {
     });
 }
 startBot();
->>>>>>> 627ea3a (update script bot)
